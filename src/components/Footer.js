39152d6901e3d8b6c6de060b0de368985385cda8
Alex Spencer <alexander.j.spencer@gmail.com>
2019-10-29 14:23:43 -0700

    init

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styles from './Color.css';
+
+const Footer = () => {
+  return (
+    <footer></footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styles from './Color.css';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Best Characters</h1>
+    </header>
+  );
+};
+
+export default Header;
