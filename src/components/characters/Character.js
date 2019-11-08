969471a0d7466baa15f6bddb04f28f718d482d63
Alex Spencer <alexander.j.spencer@gmail.com>
2019-11-08 11:50:00 -0800

    added app.test

--- src/components/characters/Character.js
+++ src/components/characters/Character.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Character.css';
 
-const Character = ({ img, name, species }) => {
-  return (
-    <li className={styles.Character}>
-      <img src={img} />
-      <div className={styles.characterInfo}>
-        <h2>{name}</h2>
-        <p>Species: {species}</p>
-      </div>
-    </li>
-  );
-};
+export default class Character extends Component {
 
-Character.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  species: PropTypes.string.isRequired
-};
+  state = {
+    name: 'Armothy'
+  }
+  
+  static propTypes = {
+    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    species: PropTypes.string.isRequired
+  };
 
-export default Character;
+  render() {
+    const { img, name, species } = this.props;
+
+    return (
+      <li className={styles.Character}>
+        <img src={img} />
+        <div className={styles.characterInfo}>
+          <h2>{name}</h2>
+          <p>Species: {species}</p>
+        </div>
+      </li>
+    );
+  }
+}
+
+// const Character = ({ img, name, species }) => {
+//   return (
+//     <li className={styles.Character}>
+//       <img src={img} />
+//       <div className={styles.characterInfo}>
+//         <h2>{name}</h2>
+//         <p>Species: {species}</p>
+//       </div>
+//     </li>
+//   );
+// };
+
+// Character.propTypes = {
+//   img: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   species: PropTypes.string.isRequired
+// };
+
+// export default Character;
